1f3fd4df3d20f54893cf613bb66bb827522ba65c
J. Bromley <jay@appliedinvention.com>
2023-11-21 23:26:42 -0800

    Refactored Supply Stacks code
    Eliminated some duplicate code by allowing the move boxes algorithm to
    be provided to the move stacks function.

--- 2022/supply_stacks/lib/supply_stacks.ex
+++ 2022/supply_stacks/lib/supply_stacks.ex
@@ -17,24 +17,12 @@ defmodule SupplyStacks do
     {stacks, moves}
   end
 
-  def move_stacks(input_file) do
+  def move_stacks(input_file, mover) do
     {stacks, moves} = read_input(input_file)
 
     moves
     |> List.foldl(stacks, fn %{num: n, start_pos: s, end_pos: e}, stks ->
-      move_boxes(stks, n, s, e)
-    end)
-    |> Map.to_list()
-    |> List.keysort(0)
-    |> Enum.map_join("", fn {_stack, boxes} -> List.first(boxes) end)
-  end
-
-  def multi_move_stacks(input_file) do
-    {stacks, moves} = read_input(input_file)
-
-    moves
-    |> List.foldl(stacks, fn %{num: n, start_pos: s, end_pos: e}, stks ->
-      move_multi_boxes(stks, n, s, e)
+      mover.(stks, n, s, e)
     end)
     |> Map.to_list()
     |> List.keysort(0)
@@ -42,10 +30,11 @@ defmodule SupplyStacks do
   end
 
   def move_boxes(stacks, num, start_pos, end_pos) do
-    case num do
-      0 -> stacks
-      _ -> move_boxes(move_box(stacks, start_pos, end_pos), num - 1, start_pos, end_pos)
-    end
+    {moved_boxes, remaining_boxes} = Map.get(stacks, start_pos) |> Enum.split(num)
+
+    stacks
+    |> Map.replace!(start_pos, remaining_boxes)
+    |> Map.update!(end_pos, fn stack -> Enum.reverse(moved_boxes) ++ stack end)
   end
 
   def move_multi_boxes(stacks, num, start_pos, end_pos) do
@@ -54,14 +43,6 @@ defmodule SupplyStacks do
     stacks
     |> Map.replace!(start_pos, remaining_boxes)
     |> Map.update!(end_pos, fn stack -> moved_boxes ++ stack end)
-  end
-
-  defp move_box(stacks, start_pos, end_pos) do
-    [box | rest_stack] = Map.get(stacks, start_pos)
-
-    stacks
-    |> Map.replace!(start_pos, rest_stack)
-    |> Map.update!(end_pos, fn stack -> [box | stack] end)
   end
 
   defp parse_stacks(stacks_desc, stack \\ %{}) do
