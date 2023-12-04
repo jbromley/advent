9be84740bc2dfbcbe807fc396f6ac6f4873f7187
J. Bromley <jay@appliedinvention.com>
2023-12-04 15:41:06 -0800

    Move reading input to main functions in Trebucht
    This allows for more accurate benchmarking.

--- 2023/trebuchet/lib/trebuchet.ex
+++ 2023/trebuchet/lib/trebuchet.ex
@@ -19,32 +19,34 @@ defmodule Trebuchet do
 
   def main(args) do
     if Enum.empty?(args) do
-      IO.puts("Usage: trebuchet input_file")
+      IO.puts("Usage: trebuchet input_file_file")
       System.halt(1)
     end
 
-    input = read_input(Enum.at(args, 0))
+    input_file = Enum.at(args, 0)
 
     IO.puts("Day 1 - Trebuchet")
-    IO.puts("Part 1: #{calculcate_code_1(input)}")
-    IO.puts("Part 2: #{calculcate_code_2(input)}")
+    IO.puts("Part 1: #{calculcate_code_1(input_file)}")
+    IO.puts("Part 2: #{calculcate_code_2(input_file)}")
   end
 
   @doc """
   Calculates the calibration code for part 1 of the problem.
   """
-  @spec calculcate_code_1([String.t()]) :: integer()
-  def calculcate_code_1(input) do
-    input
+  @spec calculcate_code_1(String.t()) :: integer()
+  def calculcate_code_1(input_file) do
+    input_file
+    |> read_input()
     |> calibration_code(&extract_calibration_number(&1, false))
   end
 
   @doc """
   Calculates the calibration code for part 2 of the problem.
   """
-  @spec calculcate_code_2([String.t()]) :: integer()
-  def calculcate_code_2(input) do
-    input
+  @spec calculcate_code_2(String.t()) :: integer()
+  def calculcate_code_2(input_file) do
+    input_file
+    |> read_input()
     |> calibration_code(&extract_calibration_number(&1, true))
   end
 
