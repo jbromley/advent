a5d2a70720fbb2d637e2a7a6d38185909b8c9727
J. Bromley <jay@appliedinvention.com>
2023-12-13 13:20:50 -0800

    Solved part 2 of AoC 2023 day 11 - Cosmic Expansion

--- 2023/cosmic-expansion/cosmic-expansion.rkt
+++ 2023/cosmic-expansion/cosmic-expansion.rkt
@@ -3,67 +3,67 @@
 (require racket/list)
 (require racket/match)
 (require racket/port)
+(require racket/set)
 (require racket/string)
 
-;;; Part 1
+;;; Part 1 and 2 (only expansion changes)
 
-(define/contract (total-distance input-file)
-  (-> string? integer?)
-  (let* ([map-in (expand (read-input input-file))]
-         [galaxies (chart-galaxies map-in)])
-    (for/fold ([distance 0])
+(define/contract (total-distance input-file expansion)
+  (-> string? integer? integer?)
+  (let ([galaxies (read-input input-file expansion)])
+    (sum-distances galaxies)))
+
+;;; Common code
+
+(define/contract (sum-distances galaxies)
+  (-> list? integer?)
+  (for/fold ([distance 0])
               ([galaxy-pairs (combinations galaxies 2)])
       (match galaxy-pairs
         [(list (cons r1 c1) (cons r2 c2))
-         (+ distance (+ (abs (- r2 r1)) (abs (- c2 c1))))]))))
-
-;;; Common code
-(define/contract (expand-vertical map-in map-out)
-   (-> list? list? list?)
-   (match map-in
-     ['() (reverse map-out)]
-     [(cons row rows)
-      (let ([new-map-out (cons row map-out)])
-        (if (non-empty-string? (string-replace row "." ""))
-          (expand-vertical rows new-map-out)
-          (expand-vertical rows (cons row new-map-out))))]))
-
-(define/contract (expand-horizontal map-in)
-  (-> list? list?)
-  (transpose (expand-vertical (transpose map-in) '())))
-
-(define/contract (expand map-in)
-  (-> list? list?)
-  (expand-horizontal (expand-vertical map-in '())))
+         (+ distance (+ (abs (- r2 r1)) (abs (- c2 c1))))])))
   
-(define/contract (read-input input-file)
-  (-> string? list?)
-  (string-split 
-    (port->string (open-input-file input-file) #:close? #t) "\n" #:trim? #t))
+(define/contract (read-input input-file expansion)
+  (-> string? integer? list?)
+  (let ([map-in (port->lines (open-input-file input-file) #:line-mode 'linefeed)])
+    (chart-galaxies map-in expansion)))        
 
-(define/contract (transpose lst)
-  (-> list? list?)
-  (let ([s-list (map string->list lst)])
-    (map list->string (apply map list s-list))))
+(define/contract (chart-galaxies map-in expansion)
+  (-> list? integer? list?)
+  (let ([num-columns (string-length (first map-in))])
+    (for*/fold ([galaxy-map '()]
+                [row 0]
+                [empty-columns (list->set (in-range num-columns))]
+                #:result (expand-horizontal galaxy-map empty-columns expansion))
+               ([line map-in])
+      (let* ([galaxy-columns (map car (regexp-match-positions* #rx"#" line))]
+             [new-galaxies (map (lambda (col) (cons row col)) galaxy-columns)]
+             [row-increment (if (empty? galaxy-columns) expansion 1)])
+        (values (append galaxy-map new-galaxies)
+                (+ row row-increment)
+                (set-subtract empty-columns (list->set galaxy-columns)))))))
 
-(define/contract (chart-galaxies map-in)
-  (-> list? list?)
-  (for/fold ([galaxies '()])
-            ([row map-in]
-             [index (in-naturals)])
-    (let ([new-galaxies (map (lambda (col) (cons index col)) (map (lambda (cell) (car cell)) (regexp-match-positions* #rx"#" row)))])
-      (values (append galaxies new-galaxies)))))
-
+(define/contract (expand-horizontal map-in empty-columns expansion)
+  (-> list? set? integer? list?)
+  (for/fold ([chart map-in]
+             #:result (sort chart < #:key car))
+            ([column (set->list empty-columns)])
+    (let-values ([(shifted fixed) (partition (lambda (pos) (> (cdr pos) column)) chart)])
+      (append fixed (map (lambda (pos) (cons (car pos) (+ (cdr pos) (- expansion 1)))) shifted)))))
+  
 ;;; Entry point
-(total-distance "input.txt")
+;; (total-distance "input.txt")
 
 ;;; Tests
 (module+ test
   (require rackunit)
 
-  (define test-cases '(("input1.txt" 374)))
+  (define test-cases '(("input1.txt" 2 374)
+                       ("input1.txt" 10 1030)
+                       ("input1.txt" 100 8410)))
 
   (for ([test test-cases])
     (let ([input-file (first test)]
-          [result (second test)])
-      (check-equal? (total-distance input-file) result))))
+          [expansion (second test)]
+          [result (third test)])
+      (check-equal? (total-distance input-file expansion) result))))
