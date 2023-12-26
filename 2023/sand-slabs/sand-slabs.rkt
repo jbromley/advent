5e3848e19971ef04718972192736168f0bbfb2b7
J. Bromley <jay@appliedinvention.com>
2023-12-26 15:45:01 -0800

    Solved AoC 2023 day 22 part 1 - Sand Slabs

--- 2023/sand-slabs/sand-slabs.rkt
+++ 2023/sand-slabs/sand-slabs.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require racket/hash)
 
 (define (brick-zs brick)
   (list (vector-ref brick 2) (vector-ref brick 5)))
@@ -19,7 +20,7 @@
 
 (define (make-space dims)
   (match-let ([(list x y z) dims])
-    (build-vector z (λ (altitude) (build-vector y (λ (row) (make-vector x 'none)))))))
+    (build-vector z (λ (altitude) (build-vector y (λ (row) (make-vector x #f)))))))
 
 (define (space-ref space x y z)
   (vector-ref (vector-ref (vector-ref space z) y) x))
@@ -33,14 +34,13 @@
       (for*/fold ([bricks '()]
                   [bricks-by-z (hash)]
                   [dims '(0 0 0)]
-                  [id 1]
+                  [id 0]
                   #:result (list
                             (list->vector (reverse bricks))
                             bricks-by-z
                             (map-space (make-space dims) (reverse bricks))))
                  ([line (in-lines port)])
         (let ([brick (list->vector (map string->number (string-split line #rx"[,~]")))])
-          (display brick)
           (values (cons brick bricks)
                   (hash-update bricks-by-z (apply min (brick-zs brick)) (λ (bricks) (cons id bricks)) (list))
                   (update-dims dims brick)
@@ -54,14 +54,53 @@
 
 (define (map-space space bricks)
   (for ([brick bricks]
-        [index (in-naturals 1)])
+        [index (in-naturals)])
     (brick-apply brick (λ (x y z) (space-set! space x y z index))))
-  space) 
+  space)
+
+;;; Part 1
+
+(define (count-breakable-bricks bricks bricks-by-z space)
+  (let ([deps (find-bricks-supports bricks bricks-by-z space)])
+    (- (vector-length bricks) (set-count (foldl (λ (s acc) (set-union! acc s) acc) (mutable-set) (filter (λ (s) (= 1 (set-count s))) (hash-values deps)))))))
+
+(define (find-bricks-supports bricks bricks-by-z space)
+  (for/fold ([deps (hash)])
+            ([z (sort (hash-keys bricks-by-z) <)])
+    (hash-union deps
+                (for/hash ([id (hash-ref bricks-by-z z)])
+                  (let ([brick (vector-ref bricks id)])
+                    (find-brick-support brick id space))))))
+
+(define (find-brick-support brick id space)
+  (let ([orig-brick (vector-copy brick)]
+        [supporters (mutable-set)])
+    (for ([_ (in-naturals)])
+      #:break (or (zero? (vector-ref brick 2))
+                  (zero? (vector-ref brick 5))
+                  (not (set-empty? supporters)))
+      (vector-set! brick 2 (sub1 (vector-ref brick 2)))
+      (vector-set! brick 5 (sub1 (vector-ref brick 5)))
+      (brick-apply brick (λ (x y z)
+                           (when (and (space-ref space x y z)
+                                      (not (= id (space-ref space x y z))))
+                                 (set-add! supporters (space-ref space x y z))))))
+    (vector-set! brick 2 (add1 (vector-ref brick 2)))
+    (vector-set! brick 5 (add1 (vector-ref brick 5)))
+    (when (not (eq? brick orig-brick))
+      (brick-apply orig-brick (λ (x y z) (space-set! space x y z #f)))
+      (brick-apply brick (λ (x y z) (space-set! space x y z id))))
+    (values id supporters)))
+      
+      
+      
+  
+    
     
 
 ;;; Entry point
 
-(define results (read-slabs "input1.txt"))
+(define results (read-slabs "input.txt"))
 (define bricks (first results))
 (define by-z (second results))
 (define space (third results))
