678e41676e31984755d85ba05c450de128122c72
J. Bromley <jay@appliedinvention.com>
2023-12-27 16:31:11 -0800

    Solved AoC 2023 day 22 - Sand Slabs

--- 2023/sand-slabs/sand-slabs.rkt
+++ 2023/sand-slabs/sand-slabs.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(require racket/hash)
+(require data/queue)
 
 (define (brick-zs brick)
   (list (vector-ref brick 2) (vector-ref brick 5)))
@@ -27,8 +27,8 @@
 
 (define (brick-move! space id start-pos end-pos)
   (when (not (eq? start-pos end-pos))
-      (brick-apply start-pos (λ (x y z) (space-set! space x y z #f)))
-      (brick-apply end-pos (λ (x y z) (space-set! space x y z id)))))
+    (brick-apply start-pos (λ (x y z) (space-set! space x y z #f)))
+    (brick-apply end-pos (λ (x y z) (space-set! space x y z id)))))
 
 (define (update-dims dims brick)
   (list (max (first dims) (add1 (vector-ref brick 0)) (add1 (vector-ref brick 3)))
@@ -37,7 +37,7 @@
 
 (define (make-space dims)
   (match-let ([(list x y z) dims])
-    (build-vector z (λ (altitude) (build-vector y (λ (row) (make-vector x #f)))))))
+    (build-vector z (λ (_) (build-vector y (λ (_) (make-vector x #f)))))))
 
 (define (space-ref space x y z)
   (vector-ref (vector-ref (vector-ref space z) y) x))
@@ -46,7 +46,7 @@
   (vector-set! (vector-ref (vector-ref space z) y) x value))
 
 (define (read-slabs input-file)
-  (call-with-input-file input-file 
+  (call-with-input-file input-file
     (lambda (port)
       (for*/fold ([bricks '()]
                   [bricks-by-z (hash)]
@@ -79,8 +79,8 @@
   (for*/fold ([bricks-under (hash)]
               [bricks-on-top (hash)]
               #:result (list bricks-under bricks-on-top))
-            ([z (sort (hash-keys bricks-by-z) <)]
-             [id (hash-ref bricks-by-z z)])
+             ([z (sort (hash-keys bricks-by-z) <)]
+              [id (hash-ref bricks-by-z z)])
     (let* ([brick (vector-ref bricks id)]
            [supports (find-brick-support brick id space)])
       (values (hash-set bricks-under id supports)
@@ -91,14 +91,13 @@
   (let ([orig-brick (vector-copy brick)]
         [supports (mutable-set)])
     (for ([_ (in-naturals)])
-         #:break (or (zero? (vector-ref brick 2))
-                     (zero? (vector-ref brick 5))
-                     (not (set-empty? supports)))
+      #:break (or (brick-hit-ground? brick)
+                  (not (set-empty? supports)))
       (brick-down! brick)
       (brick-apply brick (λ (x y z)
                            (when (and (space-ref space x y z)
                                       (not (= id (space-ref space x y z))))
-                                 (set-add! supports (space-ref space x y z))))))
+                             (set-add! supports (space-ref space x y z))))))
     (brick-up! brick)
     (brick-move! space id orig-brick brick)
     supports))
@@ -123,23 +122,52 @@
 
 ;;; Part 2
 
-(define (count-bricks-to-fall bricks bricks-by-z space)
+(define (count-all-bricks-to-fall bricks bricks-by-z space)
   (match-let ([(list bricks-under bricks-on-top)
-               (find-bricks-supports bricks bricks-by-z space)])
-    (printf "under = ~a~non top = ~a~n" bricks-under bricks-on-top)))
-    
+               (find-bricks-supports bricks bricks-by-z space)]
+              [q (make-queue)])
+    (for/sum ([brick (in-range (vector-length bricks))])
+      (count-bricks-to-fall (update-queue q brick bricks-on-top)
+                            bricks-on-top
+                            bricks-under
+                            (set brick)))))
+      
 
+(define (count-bricks-to-fall brick-queue bricks-on-top bricks-under [fallen (set)])
+  (cond
+    [(queue-empty? brick-queue)
+     (sub1 (set-count fallen))]
+    [else
+     (let ([brick (dequeue! brick-queue)])
+       (if (for/and ([dep (hash-ref bricks-under brick)])
+             (set-member? fallen dep))
+           (count-bricks-to-fall (update-queue brick-queue brick bricks-on-top) bricks-on-top bricks-under (set-add fallen brick))
+           (count-bricks-to-fall brick-queue bricks-on-top bricks-under fallen)))]))
 
-(define inputs (read-slabs "input1.txt"))
+(define (update-queue q brick bricks-on-top)
+  (for ([brick (hash-ref bricks-on-top brick (list))])
+    (when brick (enqueue! q brick)))
+  q)
+
+;;; Entry point
+
+(define inputs (read-slabs "input.txt"))
 (define bricks (first inputs))
 (define by-z (second inputs))
 (define space (third inputs))
-     
+(define g (find-bricks-supports bricks by-z space))
+(define under (first g))
+(define on-top (second g))
+(define q (make-queue))
+(enqueue! q 1)
+(enqueue! q 2)
+
+
 (module+ main
   (let* ([inputs (read-slabs "input.txt")]
          [bricks (first inputs)]
          [by-z (second inputs)]
          [space (third inputs)])
-  (printf "AoC 2023 Day 22 - Sand Slabs~n")
-  (printf "Part 1: ~a~n" (count-breakable-bricks bricks by-z space))))
-  
+    (printf "AoC 2023 Day 22 - Sand Slabs~n")
+    (printf "Part 1: ~a~n" (count-breakable-bricks bricks by-z space))))
+
