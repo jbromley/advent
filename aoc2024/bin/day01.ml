bf095108eff7057b64111384f9eac719a0460227
J. Bromley <jbromley@gmail.com>
2024-12-02 19:16:01 -0800

    Change path of input file
    Changed the path to the puzzle input file so the executable could be easily run
    with dune exec directly from the project root.

--- aoc2024/bin/day01.ml
+++ aoc2024/bin/day01.ml
@@ -50,7 +50,7 @@ let similarity l1 l2 =
 
 (* Main entry point *)
 let () =
-  let l1, l2 = read_lists "../input/1.txt" in
+  let l1, l2 = read_lists "./input/1.txt" in
   Printf.printf "Day 1: Historian Hysteria\n";
   Printf.printf "distance = %d\n" (list_dist l1 l2);
   Printf.printf "similarity = %d\n" (similarity l1 l2)
