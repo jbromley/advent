e979b602648d1e8b170065324843c5ae4583f9bf
J. Bromley <jbromley@gmail.com>
2024-12-05 09:00:54 -0800

    WIP Added functions to print a graph and do DFS

--- aoc2024/bin/day05.ml
+++ aoc2024/bin/day05.ml
@@ -16,6 +16,7 @@ let read_input name : string list * string list =
 
 (** Alias for an adjacency list graph. *)
 module Graph = Map.Make(Int)
+module IntSet = Set.Make(Int)
 
 (** Add an edge to a graph. *)
 let add_edge graph src dst =
@@ -28,6 +29,13 @@ let add_edge graph src dst =
 (** Get the neighbors of a node in a graph. *)
 let neighbors g node =
   Graph.find_opt node g
+
+(** Print the edges in a graph. *)
+let print_edges g =
+  List.iter (fun (src, dst) ->
+      let dst_nodes = List.map string_of_int dst in
+      Printf.printf "%u -> [%s]\n" src (String.concat "; " dst_nodes)
+  ) (Graph.to_seq g |> List.of_seq)
 
 (** Parse the rules graph text to build a graph. *)
 let parse_graph input =
@@ -49,6 +57,30 @@ let parse_updates input =
       add_update ls (update :: updates)
   in
   add_update input []
+
+(** Run depth-first search to see if node dst follows node src in the graph.
+    If there is a path from src to dst, them return Some(path) where path
+    is a list of nodes in the path. If there is no path, return None. *)
+let dfs graph src dst =
+  let rec explore visited path current =
+    if current = dst then
+      Some (List.rev (current :: path)) (* Path found, reverse it to return in correct order *)
+    else if IntSet.mem current visited then
+      None (* Already visited, no path here *)
+    else
+      let visited = IntSet.add current visited in
+      let neighbors = Graph.find_opt current graph |> Option.value ~default:[] in
+      let rec try_neighbors = function
+        | [] -> None (* No neighbors lead to the destination *)
+        | neighbor :: rest -> (
+            match explore visited (current :: path) neighbor with
+            | Some path -> Some path (* Path found via this neighbor *)
+            | None -> try_neighbors rest (* Try the next neighbor *)
+          )
+      in
+      try_neighbors neighbors
+  in
+  explore IntSet.empty [] src
   
 let run () =
   let rules_text, updates_text = read_input "./input/05.txt" in 
