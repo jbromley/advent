1b438d5fd0cf1ed21708be7d700904ff77dcd4e9
J. Bromley <jay@appliedinvention.com>
2024-12-05 13:19:32 -0800

    WIP Completed incorrect solution for part 1
    The solution runs, but the result it returns is too large.

--- aoc2024/bin/day05.ml
+++ aoc2024/bin/day05.ml
@@ -1,18 +1,8 @@
 (* Day 5: Print Queue *)
 
-(** Read the input text for rules and updates. *)
-let read_input name : string list * string list =
-  let ic = open_in name in 
-  let try_read () = try Some(input_line ic) with End_of_file -> None in
-  let rec loop rules updates do_rules =
-    match try_read () with
-    | Some s ->
-      if s = "" then loop rules updates false
-      else if do_rules then loop (s :: rules) updates true
-      else loop rules (s :: updates) false
-    | None ->
-      close_in ic; (rules, updates) in
-  loop [] [] true
+(*******************************)
+(* Graph modules and functions *)
+(*******************************)
 
 (** Alias for an adjacency list graph. *)
 module Graph = Map.Make(Int)
@@ -37,27 +27,6 @@ let print_edges g =
       Printf.printf "%u -> [%s]\n" src (String.concat "; " dst_nodes)
   ) (Graph.to_seq g |> List.of_seq)
 
-(** Parse the rules graph text to build a graph. *)
-let parse_graph input =
-  let rec add_node edges g =
-    match edges with
-    | [] -> g
-    | e :: es ->
-      let new_g = Scanf.sscanf e "%u|%u" (fun src dst -> add_edge g src dst) in
-      add_node es new_g
-  in
-  add_node input Graph.empty
-
-let parse_updates input =
-  let rec add_update lines updates =
-    match lines with
-    | [] -> updates
-    | l :: ls ->
-      let update = String.split_on_char ',' l |> (List.map int_of_string) in
-      add_update ls (update :: updates)
-  in
-  add_update input []
-
 (** Run depth-first search to see if node dst follows node src in the graph.
     If there is a path from src to dst, them return Some(path) where path
     is a list of nodes in the path. If there is no path, return None. *)
@@ -81,9 +50,83 @@ let dfs graph src dst =
       try_neighbors neighbors
   in
   explore IntSet.empty [] src
+
+(*****************************)
+(* Input reading and parsing *)
+(*****************************)
+
+(** Read the input text for rules and updates. *)
+let read_input name : string list * string list =
+  let ic = open_in name in 
+  let try_read () = try Some(input_line ic) with End_of_file -> None in
+  let rec loop rules updates do_rules =
+    match try_read () with
+    | Some s ->
+      if s = "" then loop rules updates false
+      else if do_rules then loop (s :: rules) updates true
+      else loop rules (s :: updates) false
+    | None ->
+      close_in ic; (rules, updates) in
+  loop [] [] true
+
+(** Parse the rules graph text to build a graph. *)
+let parse_graph input =
+  let rec add_node edges g =
+    match edges with
+    | [] -> g
+    | e :: es ->
+      let new_g = Scanf.sscanf e "%u|%u" (fun src dst -> add_edge g src dst) in
+      add_node es new_g
+  in
+  add_node input Graph.empty
+
+let parse_updates input =
+  let rec parse_one lines updates =
+    match lines with
+    | [] -> updates
+    | l :: ls ->
+      let update = String.split_on_char ',' l |> (List.map int_of_string) |> Array.of_list in
+      parse_one ls (update :: updates)
+  in
+  parse_one input []
+
+
+(********************)
+(* Puzzle solutions *)
+(********************)
+
+(** Check if an update is valid. *)
+let is_update_valid update rules =
+  let update_list = Array.to_list update in
+  let rec check_node nodes src =
+    match nodes with
+    | [] -> true
+    | dst :: tail_nodes ->
+      match dfs rules src dst with
+      | Some _ -> check_node tail_nodes dst
+      | None -> false
+  in
+  check_node (List.tl update_list) (List.hd update_list)
+
+(** Part 1: Check which updates are valid and sum the middle pages of the
+    valid updates. *)
+let sum_valid_updates updates rules =
+  let rec check_update update sum =
+    match update with
+    | [] -> sum
+    | u :: us ->
+      if is_update_valid u rules then
+        check_update us (sum + u.(Array.length u / 2))
+      else
+        check_update us sum
+  in
+  check_update updates 0
+    
   
 let run () =
-  let rules_text, updates_text = read_input "./input/05.txt" in 
-  Printf.printf "Day 4: Print Queue\n";
-  (* Printf.printf "count \"XMAS\" = %d\n" (count_all_xmas input);
-     Printf.printf "count \"MAS\" crosses = %d\n" (count_all_mas_crosses input) *)
+  let rules_text, updates_text = read_input "./input/05.txt" in
+  let rules = parse_graph rules_text in
+  let updates = parse_updates updates_text in 
+  Printf.printf "Day 5: Print Queue\n";
+  Printf.printf "safe page sum = %d\n" (sum_valid_updates updates rules);
+  (* Printf.printf "count \"MAS\" crosses = %d\n" (count_all_mas_crosses input) *)
