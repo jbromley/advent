9574f27b8bf8a98b0b7e91fe775bdd272c1f51e2
J. Bromley <jbromley@gmail.com>
2024-12-11 01:06:21 -0800

    Solved day 10, part 2
    Used a Map instead of a color matrix for tracking nodes. Added ability to count
    multiple paths to the same end of trail.

--- aoc2024/lib/day10.ml
+++ aoc2024/lib/day10.ml
@@ -1,12 +1,17 @@
 (* Day 10: Hoof It *)
 
-type map = int array array
-
-type graph_color = White | Gray | Black
-
+module Pos = struct
+  type t = int * int
+  let compare (x1, y1) (x2, y2) =
+    match compare x1 x2 with
+    | 0 -> compare y1 y2
+    | others -> others
+end
+module PosSet = Set.Make(Pos)
+    
 let directions = [(-1, 0); (0, 1); (1, 0); (0, -1)]
 
-let read_map name : map =
+let read_map name =
   let ic = open_in name in 
   let try_read () =
     try
@@ -15,16 +20,17 @@ let read_map name : map =
   let rec loop input =
     match try_read () with
     | Some s ->
-      let row = String.to_seq s |>
-                Seq.map (fun ch -> Char.code ch - Char.code '0') |>
-                Array.of_seq
-      in
-      loop (row :: input)
+      loop (s :: input)
     | None ->
-      close_in ic; List.rev input |> Array.of_list
+      close_in ic; List.rev input
   in
   loop []
 
+let parse_map input =
+  List.map
+    (fun s -> String.to_seq s |> Seq.map (fun ch -> Char.code ch - Char.code '0') |> Array.of_seq)
+    input |> Array.of_list
+    
 let on_map map (r, c) =
   if r >= 0 && r < Array.length map &&
      c >= 0 && c < Array.length map.(0) then Some (r, c)
@@ -33,43 +39,42 @@ let on_map map (r, c) =
 let dims map =
   (Array.length map, Array.length map.(0))
 
-let print_step r c next_nodes =
-  Printf.printf "node = (%d, %d), neigbors = " r c;
+let print_step map r c next_nodes =
+  Printf.printf "node = (%d, %d)\taltitude = %d\tneigbors = " r c map.(r).(c);
   List.iter (fun (r, c) -> Printf.printf "(%d, %d) " r c) next_nodes;
   print_endline ""
     
-let bfs map r c =
-  let rec loop q color trails =
+let bfs map r c rating =
+  let rec loop q discovered trails =
     match Queue.take_opt q with
     | None -> trails
     | Some (r, c) ->
-      if map.(r).(c) = 9 then (color.(r).(c) <- Black; loop q color (trails + 1))
+      if map.(r).(c) = 9 then loop q discovered (trails + 1)
       else (
         let next_nodes = List.map (fun (dr, dc) -> (r + dr, c + dc)) directions |>
                          List.filter_map (fun pos -> on_map map pos) |>
-                         List.filter (fun (nr, nc) -> (color.(nr).(nc) = White) &&
-                                                      map.(nr).(nc) = map.(r).(c) + 1) in
-        List.iter (fun (nr, nc) -> color.(nr).(nc) <- Gray; Queue.push (nr, nc) q) next_nodes;
-        color.(r).(c) <- Black;
-        loop q color trails)
+                         List.filter (fun (nr, nc) ->
+                             map.(nr).(nc) = map.(r).(c) + 1 &&
+                             (rating || not (PosSet.mem (nr, nc) discovered))) in
+        List.iter (fun (nr, nc) -> Queue.push (nr, nc) q) next_nodes;
+        let discovered' = List.fold_left (fun acc node -> PosSet.add node acc) discovered next_nodes in
+        loop q discovered' trails)
   in
-  let rows, cols = dims map in
   let q = Queue.create () in
   Queue.push (r, c) q;
-  loop q (Array.make_matrix rows cols White) 0
+  loop q (PosSet.singleton (r, c)) 0
 
-let sum_trailhead_scores map =
+let sum_trailhead_scores map ?(rating = false) () =
   let total_score = ref 0 in
   Array.iteri
     (fun r row ->
       Array.iteri (fun c elt ->
-          if elt = 0 then let score = bfs map r c in total_score := !total_score + score) row)
+          if elt = 0 then let score = bfs map r c rating in total_score := !total_score + score) row)
     map;
   !total_score
 
 let run () =                   
-  let map = read_map "./input/10.txt" in (*  *)
+  let map = read_map "./input/10.txt" |> parse_map in
   Printf.printf "Day 10: Hoof It\n";
-  Printf.printf "trail score = %d\n" (sum_trailhead_scores map);
-(* Printf.printf "checksum, files defrag = %d\n" (defragment fs ~by_blocks:false () |> checksum) *)
-
+  Printf.printf "trail score = %d\n" (sum_trailhead_scores map ~rating:false ());
+  Printf.printf "trail rating = %d\n" (sum_trailhead_scores map ~rating:false ())
--- aoc2024/test/test_day10.ml
+++ aoc2024/test/test_day10.ml
@@ -2,26 +2,25 @@ open OUnit2
 open Aoc
 open Day10
 
-let input = [ "89010123";
-              "78121874";
-              "87430965";
-              "96549874";
-              "45678903";
-              "32019012";
-              "01329801";
-              "10456732" ]
+let map = parse_map [ "89010123";
+                      "78121874";
+                      "87430965";
+                      "96549874";
+                      "45678903";
+                      "32019012";
+                      "01329801";
+                      "10456732" ]
 
-(*
-let test_by_blocks _ = assert_equal 1928 (defragment fs ~by_blocks:true () |> checksum)
 
-let test_by_files _ = assert_equal 2858 (defragment fs ~by_blocks:false () |> checksum)
-*)
+let test_scoring _ = assert_equal 36 (sum_trailhead_scores map ~rating:false ())
+
+let test_rating _ = assert_equal 81  (sum_trailhead_scores map ~rating:true ())
          
 let suite_name = "Day 10: Hoof It"
 let suite =
   suite_name >::: [
-    (* "test defragment by blocks" >:: test_by_blocks; *)
-    (* "test defragment by files" >:: test_by_files; *)
+    "test scoring" >:: test_scoring;
+    "test rating" >:: test_rating;
   ]
 
 let () =
